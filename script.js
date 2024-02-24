54ba6e947f56865b4bd50ade54725cf1d62d0994
PradhyumShivhare <pradhyumshivhare8@gmail.com>
2024-02-24 21:46:44 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,73 @@
+function toggleMute() {
+    const audio = document.getElementById('audio');
+    const muteBtn = document.getElementById('mute-btn');
+    const unmuteBtn = document.getElementById('unmute-btn');
+
+    if (audio.muted) {
+      audio.muted = false;
+      muteBtn.style.display = 'none';
+      unmuteBtn.style.display = 'inline';
+    } else {
+      audio.muted = true;
+      muteBtn.style.display = 'inline';
+      unmuteBtn.style.display = 'none';
+    }
+    audio.play();
+  }
+
+
+
+
+
+
+
+  function toggleFullScreen() {
+    if (!document.fullscreenElement &&    
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+        toggleButtonVisibility();
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+        toggleButtonVisibility();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+        toggleButtonVisibility();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        toggleButtonVisibility();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+        toggleButtonVisibility();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+        toggleButtonVisibility();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+        toggleButtonVisibility();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+        toggleButtonVisibility();
+      }
+    }
+  }
+
+
+
+
+
+  function toggleButtonVisibility() {
+    var fullscreenButton = document.querySelector('.fullscreen-button');
+    var compressButton = document.querySelector('.compress-btn');
+
+
+    if (fullscreenButton.style.display === 'none') {
+      fullscreenButton.style.display = 'inline-block';
+      compressButton.style.display = 'none';
+    } else {
+      fullscreenButton.style.display = 'none';
+      compressButton.style.display = 'inline-block';
+    }
+  }
