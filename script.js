a453146f8d70a6512e23c0358c32872b8ddf0666
PradhyumShivhare <pradhyumshivhare8@gmail.com>
2024-02-25 22:24:23 +0530

    Added Slider Functionality + Typewriting effect in Landing Text

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
+var txt = 'The Annual Cultural Fest of Shaheed Sukhdev College of Business Studies';
+var speed = 70;
+
+function typeWriter() {
+  var i = 0;
+  var interval = setInterval(function () {
+    document.getElementById("landing-text").innerHTML += txt.charAt(i);
+    i++;
+    if (i > txt.length - 1) {
+      clearInterval(interval);
+    }
+  }, speed);
+}
+
+
 function toggleMute() {
-    const audio = document.getElementById('audio');
-    const muteBtn = document.getElementById('mute-btn');
-    const unmuteBtn = document.getElementById('unmute-btn');
+  const audio = document.getElementById('audio');
+  const muteBtn = document.getElementById('mute-btn');
+  const unmuteBtn = document.getElementById('unmute-btn');
 
-    if (audio.muted) {
-      audio.muted = false;
-      muteBtn.style.display = 'none';
-      unmuteBtn.style.display = 'inline';
-    } else {
-      audio.muted = true;
-      muteBtn.style.display = 'inline';
-      unmuteBtn.style.display = 'none';
-    }
-    audio.play();
+  if (audio.muted) {
+    audio.muted = false;
+    muteBtn.style.display = 'none';
+    unmuteBtn.style.display = 'inline';
+  } else {
+    audio.muted = true;
+    muteBtn.style.display = 'inline';
+    unmuteBtn.style.display = 'none';
   }
+  audio.play();
+}
 
 
-
-
-
-
-
-  function toggleFullScreen() {
-    if (!document.fullscreenElement &&    
-      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-        toggleButtonVisibility();
-      } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen();
-        toggleButtonVisibility();
-      } else if (document.documentElement.mozRequestFullScreen) {
-        document.documentElement.mozRequestFullScreen();
-        toggleButtonVisibility();
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-        toggleButtonVisibility();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-        toggleButtonVisibility();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-        toggleButtonVisibility();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-        toggleButtonVisibility();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-        toggleButtonVisibility();
-      }
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&
+    !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+      toggleButtonVisibility();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+      toggleButtonVisibility();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+      toggleButtonVisibility();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      toggleButtonVisibility();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+      toggleButtonVisibility();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+      toggleButtonVisibility();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+      toggleButtonVisibility();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+      toggleButtonVisibility();
     }
   }
+}
 
 
 
 
 
-  function toggleButtonVisibility() {
-    var fullscreenButton = document.querySelector('.fullscreen-button');
-    var compressButton = document.querySelector('.compress-btn');
+function toggleButtonVisibility() {
+  var fullscreenButton = document.querySelector('.fullscreen-button');
+  var compressButton = document.querySelector('.compress-btn');
 
 
-    if (fullscreenButton.style.display === 'none') {
-      fullscreenButton.style.display = 'inline-block';
-      compressButton.style.display = 'none';
-    } else {
-      fullscreenButton.style.display = 'none';
-      compressButton.style.display = 'inline-block';
-    }
+  if (fullscreenButton.style.display === 'none') {
+    fullscreenButton.style.display = 'inline-block';
+    compressButton.style.display = 'none';
+  } else {
+    fullscreenButton.style.display = 'none';
+    compressButton.style.display = 'inline-block';
   }
+}
