2d042632c66eb1ec305a73298c0e6a5783d0b28c
PradhyumShivhare <pradhyumshivhare8@gmail.com>
2024-02-28 23:31:56 +0530

    Comic loader built complete
    Need to integrate in main index.html

--- motion-comic/comic.js
+++ motion-comic/comic.js
@@ -1,24 +1,24 @@
-
-function animateBlickDots() {
-    const dotsElements = document.querySelectorAll('.blick-dots');
+function animateBlinkDots() {
+    const dotsElements = document.querySelectorAll('.blink-dots');
     dotsElements.forEach(dotsElement => {
         let count = 0;
-        const maxCount = 4; 
+        const maxCount = 4;
 
         setInterval(() => {
-            const dots = '.'.repeat(count % 4); 
+            const dots = '.'.repeat(count % 4);
             count++;
             dotsElement.textContent = dots;
-        }, 400); 
+        }, 400);
     });
 }
 
 
 
-var txt = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-var speed = 70;
+
 
 function typeWriterleft1() {
+    var txt = 'l1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+    var speed = 70;
     var i = 0;
     var interval = setInterval(function () {
         document.getElementById("message-textbox-left-1").innerHTML += txt.charAt(i);
@@ -27,12 +27,13 @@ function typeWriterleft1() {
             clearInterval(interval);
         }
     }, speed);
+
 }
 
-var txt = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-var speed = 70;
-
 function typeWriterleft2() {
+    var txt = 'l2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+    var speed = 70;
+
     var i = 0;
     var interval = setInterval(function () {
         document.getElementById("message-textbox-left-2").innerHTML += txt.charAt(i);
@@ -43,10 +44,11 @@ function typeWriterleft2() {
     }, speed);
 }
 
-var txt = 'Lorem Ipsum is simply duewfewmmy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
-var speed = 70;
+
 
 function typeWriterright() {
+    var txt = 'r1Lorem Ipsum is simply duewfewmmy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+    var speed = 70;
     var i = 0;
     var interval = setInterval(function () {
         document.getElementById("message-textbox-right").innerHTML += txt.charAt(i);
@@ -60,9 +62,34 @@ function typeWriterright() {
 
 
 
-window.onload = function() {
+window.onload = function () {
+    document.querySelector('.loading-txt').style.display = 'block'; 
+    
+    setTimeout(function () {
+        document.querySelector('.loader-bg-img').classList.add('loaded');
+        document.querySelector('.loading-txt').style.display = 'none';
+        document.querySelector('#next-btn-1').style.display = 'block';
+        document.querySelector('.left-char-con-1').style.display = 'flex';
+        typeWriterleft1("message-textbox-left-1");
+    }, 4000);
+}
+animateBlinkDots("blink-dots");
+document.getElementById('next-btn-1').onclick = function () {
+    document.querySelector('#next-btn-1').style.display = 'none';
+    document.querySelector('#next-btn-2').style.display = 'block';
+    document.querySelector('.left-char-con-1').style.display = 'none';
+    document.querySelector('.right-char-con-1').style.display = 'flex';
     typeWriterright("message-textbox-right");
-    typeWriterleft1("message-textbox-left-1");
+}
+
+document.getElementById('next-btn-2').onclick = function () {
+    document.querySelector('#next-btn-2').style.display = 'none';
+    document.querySelector('#teleport-btn').style.display = 'block';
+    document.querySelector('.left-char-con-2').style.display = 'flex';
+    document.querySelector('.right-char-con-1').style.display = 'none';
     typeWriterleft2("message-textbox-left-2");
-    animateBlickDots("blink-dots");
-};
+}
+
+document.getElementById('teleport-btn').onclick = function () {
+    window.location.href = '/crescendo-web/index.html';
+}
